eb566d29d15870d02124dc0474f062685465f0c3
Eugene Oparyshev <eop@sitecore.net>
2017-10-16 20:24:11 +0300

    Github patch

---
+++ src/Sitecore.Support.92188/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.92188")]
+[assembly: AssemblyProduct("Sitecore.Support.92188")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.92188/shell/applications/webedit/dialogs/dateselector/DateSelectorPage.cs
@@ -0,0 +1,20 @@
+﻿namespace Sitecore.Support.shell.applications.webedit.dialogs.dateselector
+{
+    using Sitecore.Web.UI.Sheer;
+    using System;
+    public class DateSelectorPage : Sitecore.Shell.Applications.WebEdit.Dialogs.DateSelector.DateSelectorPage
+    {
+        protected override void OK_Click()
+        {
+            if (base.Calendar1.SelectedDate != new DateTime(0L))
+            {
+                base.OK_Click();
+            }
+            else
+            {
+                SheerResponse.CloseWindow();
+            }
+        }
+
+    }
+}
